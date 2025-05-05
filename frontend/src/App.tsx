c5abfa3900d795000e4e87c3d4db80135eb4b68c
Jack Buehner <jackb@furman.edu>
2025-05-05 15:51:59 -0400

    feat(frontend): add a page to see all components when in dev mode

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,23 @@
+import styled from '@emotion/styled';
+import { Route, Routes } from 'react-router';
+import { DevModeComponentsAll } from './views';
+
 export default function App() {
   return (
     <>
-      <h1>Greenville Connects Mobility Dashboard</h1>
+      {import.meta.env.DEV ? <PlaceholderGreenvilleConnectsWebsiteHeader /> : null}
+      <Routes>
+        <Route path="/" element={<h1>Greenville Connects Mobility Dashboard</h1>} />
+        {import.meta.env.DEV ? (
+          <Route path="/components" element={<DevModeComponentsAll />} />
+        ) : null}
+      </Routes>
     </>
   );
 }
+
+const PlaceholderGreenvilleConnectsWebsiteHeader = styled.div`
+  block-size: 4rem;
+  inline-size: 100%;
+  background-color: var(--color-green2);
+`;
---
+++ frontend/src/views/index.ts
@@ -0,0 +1,1 @@
+export { DevModeComponentsAll } from './DevModeComponentsAll';
