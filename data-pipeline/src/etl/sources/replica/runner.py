40d380eb373e178980e087843c8678a026945277
Jack Buehner <jackb@furman.edu>
2025-05-13 09:00:05 -0400

    feat(pipeline): add parameters for filtering replica seasons
    This makes it possible to limit the range of seasons for which data is downloaded. Mostly useful for testing.

--- data-pipeline/src/etl/sources/replica/runner.py
+++ data-pipeline/src/etl/sources/replica/runner.py
@@ -1,5 +1,26 @@
+import os
+from typing import Optional
+
 from etl.sources.replica.etl import ReplicaETL
 
+
 def source_runner():
+    # print all environment variables
+    print("Environment variables:")
+    for key, value in os.environ.items():
+        print(f"{key}: {value}")
+
+    REPLICA_YEARS_FILTER = os.getenv('REPLICA_YEARS_FILTER') or None
+    years: Optional[list[int]] = None
+    if REPLICA_YEARS_FILTER is not None:
+        years = [int(year.strip()) for year in REPLICA_YEARS_FILTER.split(',')]
+
+    REPLICA_QUARTERS_FILTER = os.getenv('REPLICA_QUARTERS_FILTER') or None
+    quarters: Optional[list[str]] = None
+    if REPLICA_QUARTERS_FILTER is not None:
+        quarters = [quarter.strip()
+                    for quarter in REPLICA_QUARTERS_FILTER.split(',')]
+
     # get replica data
-    ReplicaETL(columns=['activity_id', 'person_id', 'mode', 'travel_purpose']).run()
+    ReplicaETL(['activity_id', 'person_id', 'mode',
+               'travel_purpose'], years, quarters).run()
