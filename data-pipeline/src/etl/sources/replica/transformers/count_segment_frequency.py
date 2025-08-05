2185e305b6e14786cecf69866eea6aa5f54af59c
Jack Buehner <jackb@furman.edu>
2025-08-05 15:39:38 -0400

    feat(pipeline/replica): add logging to count_segment_frequency

--- data-pipeline/src/etl/sources/replica/transformers/count_segment_frequency.py
+++ data-pipeline/src/etl/sources/replica/transformers/count_segment_frequency.py
@@ -1,7 +1,11 @@
+import logging
 from typing import Optional
 
 import geopandas
 import numpy
+
+logger = logging.getLogger('count_segment_frequency')
+logger.setLevel(logging.DEBUG)
 
 
 def count_segment_frequency(trips_gdf: geopandas.GeoDataFrame, full_segments_gdf: Optional[geopandas.GeoDataFrame] = None) -> geopandas.GeoDataFrame:
@@ -21,12 +25,16 @@ def count_segment_frequency(trips_gdf: geopandas.GeoDataFrame, full_segments_gdf
     """
     # split each multilinestring into individual linestrings
     # and create a MultiIndex with activity_id and segment_index
+    logger.debug('Exploding trips GeoDataFrame to count segment frequencies...')
     exploded = trips_gdf.set_index('activity_id').explode(index_parts=True)  # create a multiindex
     exploded.index.rename(['activity_id', 'segment_index'], inplace=True)
 
     # count how many segments each activity has
+    logger.debug('Counting segments per activity...')
     activity_segment_counts = exploded.index.to_frame(index=False).groupby(
         'activity_id')['segment_index'].max() + 1
+
+    logger.debug('Building filter...')
 
     # get the segment indices and activity ids as numpy arrays
     segment_indices = exploded.index.get_level_values('segment_index')
@@ -43,6 +51,7 @@ def count_segment_frequency(trips_gdf: geopandas.GeoDataFrame, full_segments_gdf
     filtered_exploded = exploded[filter_condition]
 
     # count the frequency of each unique segment
+    logger.debug('Counting segment frequencies...')
     segment_counts = (
         filtered_exploded.groupby([filtered_exploded.geometry])
         .size()
@@ -50,6 +59,7 @@ def count_segment_frequency(trips_gdf: geopandas.GeoDataFrame, full_segments_gdf
     )
 
     # assign each segment to bucket based on its frequency (0 = low frequency, 10 = highest frequency)
+    logger.debug('Assigning frequency buckets...')
     buckets_count = 10
     max_frequency = segment_counts['frequency'].max()
     segment_counts['frequency_bucket'] = numpy.minimum(
@@ -57,10 +67,12 @@ def count_segment_frequency(trips_gdf: geopandas.GeoDataFrame, full_segments_gdf
     )
 
     # convert to a GeoDataFrame
+    logger.debug('Converting segment counts to GeoDataFrame with fixed geometry...')
     segments_gdf = fix_geometry(geopandas.GeoDataFrame(segment_counts))
 
     # if full_segments_gdf is available, rebase the frequency data on it
     if full_segments_gdf is not None:
+        logger.debug('Rebasing segment frequencies on full segments GeoDataFrame...')
         full_segments_gdf = fix_geometry(full_segments_gdf)
         shared_segments_gdf = full_segments_gdf[full_segments_gdf.geometry.isin(
             segments_gdf.geometry)]
