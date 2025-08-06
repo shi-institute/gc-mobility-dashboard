4a8936c6d36fe0bd9f4c02942efc03af118f0208
Tanyaradzwa Masvimbo <masvta0@furman.edu>
2025-08-06 18:37:57 -0400

    feat(pipeline/census_acs_5year): add `areas` property to `time_series.json` with the area names that correspond to the associated tract (#66)
    Downloads census tract geometries for the available years for Greenville County, intersects with the area geometries, and saves an array of results in JSON. The JSON array is then merged witht the other time series data.
    
    ---------
    
    * feat: Add census tract ETL pipeline
    
    * feat(pipeline/census_acs_5year): simplify tract_etl logic, run tract_etl for all years, and merge tract_etl results with other census data results
    
    ---------
    
    Co-authored-by: Jack Buehner <jackb@furman.edu>

--- data-pipeline/src/etl/sources/census_acs_5year/runner.py
+++ data-pipeline/src/etl/sources/census_acs_5year/runner.py
@@ -1,5 +1,8 @@
+from pathlib import Path
+
 from etl.merge import merge_json_array_files
 from etl.sources.census_acs_5year.etl import CensusACS5YearEstimatesETL
+from etl.sources.census_acs_5year.tract_etl import CensusIntersectAreasETL
 
 
 def source_runner():
@@ -21,13 +24,19 @@ def source_runner():
         .prune_tables()\
         .to_time_series()
 
+    # get the areas that correspond to the census tracts
+    area_json_paths = list(
+        sorted(Path('./input/replica_interest_area_polygons').glob('*.geojson')))
+    geo = CensusIntersectAreasETL(area_json_paths).run()
+
     # merge all Census ACS 5-Year Estimates data into a single time series file
     merge_json_array_files(
         json_files=[
             s0101.time_series_file_path,
             dp05.time_series_file_path,
             s1501.time_series_file_path,
-            b08201.time_series_file_path
+            b08201.time_series_file_path,
+            geo.time_series_file_path,
         ],
         output_file='./data/census_acs_5year/time_series.json'
     )
