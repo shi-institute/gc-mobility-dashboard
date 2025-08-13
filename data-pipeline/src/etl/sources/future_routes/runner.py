e603cae0c6a30a23944631a428e3c42a2f364b1f
Jack Buehner <jackb@furman.edu>
2025-08-13 15:37:08 -0400

    feat(pipeline): add future scenarios ETL (#83)

---
+++ data-pipeline/src/etl/sources/future_routes/runner.py
@@ -0,0 +1,15 @@
+
+
+from pathlib import Path
+
+from etl.sources.future_routes.etl import FutureRoutesETL
+
+after = ['repica', 'greenlink_gtfs']
+
+
+def source_runner():
+    areas_folder = Path('./input/replica_interest_area_polygons')
+    areas_geojson_paths = [file for file in areas_folder.glob(
+        '*.geojson') if file.name != 'full_area.geojson']
+
+    FutureRoutesETL(areas_geojson_paths).run()
