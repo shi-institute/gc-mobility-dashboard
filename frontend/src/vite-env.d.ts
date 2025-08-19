d31741f01aeb6666ea1e72223bd7badfe18ad0de
Jack Buehner <jackb@furman.edu>
2025-08-19 13:44:13 -0400

    feat: support specifying a different data origin and pathname via the GCMD_DATA_ORIGIN and GCMD_DATA_PATH environment variables (#118)

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,2 +1,5 @@
 /// <reference types="vite/client" />
 /// <reference types="@arcgis/map-components/types/react" />
+
+declare const __GCMD_DATA_ORIGIN__: string;
+declare const __GCMD_DATA_PATH__: string;
