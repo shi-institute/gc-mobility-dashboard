b01474b9b70f3e60e5497695678cc6555d3a819c
mwiniski <mwiniski@gmail.com>
2025-08-20 15:07:54 -0400

    chore: load local .env file in vite.config (#122)

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,35 +1,39 @@
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
 // https://vite.dev/config/
-export default defineConfig({
-  define: {
-    __GCMD_DATA_ORIGIN__: JSON.stringify(process.env.GCMD_DATA_ORIGIN || '.'),
-    __GCMD_DATA_PATH__: JSON.stringify(process.env.GCMD_DATA_PATH ?? '/data'),
-  },
-  plugins: [
-    react({
-      jsxImportSource: '@emotion/react',
-      babel: {
-        plugins: [
-          [
-            '@emotion/babel-plugin',
-            {
-              sourceMap: true,
-              autoLabel: 'always',
-              labelFormat: '-[dirname]-[local]',
-              cssPropOptimization: true,
-            },
-          ],
-        ],
-      },
-    }),
-  ],
-  appType: 'spa',
-  base: './',
-  server: {
-    watch: {
-      ignored: ['**/node_modules/**', '**/public/data/**'],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    define: {
+      __GCMD_DATA_ORIGIN__: JSON.stringify(env.GCMD_DATA_ORIGIN || '.'),
+      __GCMD_DATA_PATH__: JSON.stringify(env.GCMD_DATA_PATH ?? '/data'),
     },
-  },
+    plugins: [
+      react({
+        jsxImportSource: '@emotion/react',
+        babel: {
+          plugins: [
+            [
+              '@emotion/babel-plugin',
+              {
+                sourceMap: true,
+                autoLabel: 'always',
+                labelFormat: '-[dirname]-[local]',
+                cssPropOptimization: true,
+              },
+            ],
+          ],
+        },
+      }),
+    ],
+    appType: 'spa',
+    base: './',
+    server: {
+      watch: {
+        ignored: ['**/node_modules/**', '**/public/data/**'],
+      },
+    },
+  };
 });
