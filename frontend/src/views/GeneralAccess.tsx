c176265e1451d9b5c9e1d3b908d358f1a00a0102
Jack Buehner <jackb@furman.edu>
2025-08-26 15:34:20 -0400

    feat(frontend): move work and school section to between service stats and area demographics on tab 1 (#134)
    Resolves #131

--- frontend/src/views/GeneralAccess.tsx
+++ frontend/src/views/GeneralAccess.tsx
@@ -199,6 +199,97 @@ function Sections() {
         })}
       />
     </Section>,
+
+    <Section title="Work and School">
+      <SectionEntry
+        s={{ gridColumn: '1 / 3' }}
+        m={{ gridColumn: '1 / 4' }}
+        l={{ gridColumn: '1 / 5' }}
+      >
+        <div>
+          <SelectTravelMethod
+            travelMethodList={travelMethodList}
+            label={
+              'Show trip density on the map for commutes in ' +
+              (data?.length === 1 ? 'this area' : 'the selected seasons and areas')
+            }
+          />
+        </div>
+      </SectionEntry>
+      <Statistic.Percent
+        wrap
+        label="Any trip using public transit"
+        data={data?.map((area) => {
+          const publicTransitTrips =
+            area.statistics?.thursday_trip.methods.commute.public_transit || 0;
+          const allTrips = Object.values(
+            area.statistics?.thursday_trip.methods.commute || {}
+          ).reduce((sum, value) => sum + (value || 0), 0);
+
+          return {
+            label: area.__label,
+            value: ((publicTransitTrips / allTrips) * 100).toFixed(2),
+          };
+        })}
+      />
+      <Statistic.Percent
+        wrap
+        label="Any trip that could use public transit"
+        data={data?.map((area) => {
+          const possibleConversions =
+            area.statistics?.thursday_trip.possible_conversions.via_walk || 0;
+          const allTrips = Object.values(area.statistics?.thursday_trip.methods.__all || {}).reduce(
+            (sum, value) => sum + (value || 0),
+            0
+          );
+
+          return {
+            label: area.__label,
+            value: ((possibleConversions / allTrips) * 100).toFixed(2),
+          };
+        })}
+      />
+      <Statistic.Percent
+        wrap
+        label="Households without a vehicle (ACS)"
+        data={data?.map((area) => {
+          const households =
+            area.census_acs_5year
+              ?.map((item) => item.households__total)
+              .reduce((sum, value) => sum + (value || 0), 0) || NaN;
+          const householdsNoVehicle =
+            area.census_acs_5year
+              ?.map((item) => item.households__no_vehicle)
+              .reduce((sum, value) => sum + (value || 0), 0) || NaN;
+
+          return {
+            label: area.__label,
+            value: ((householdsNoVehicle / households) * 100).toFixed(2),
+          };
+        })}
+      />
+      <Statistic.Number
+        wrap
+        label="Median commute time (all modes)"
+        unit="minutes"
+        data={data?.map((area) => {
+          const medianDuration = area.statistics?.thursday_trip.median_duration.commute || 0;
+          return { label: area.__label, value: medianDuration.toFixed(2) };
+        })}
+      />
+      <SectionEntry
+        s={{ gridColumn: '1 / 3' }}
+        m={{ gridColumn: '1 / 4' }}
+        l={{ gridColumn: '1 / 5' }}
+      >
+        <div>
+          <Button href={'#/job-access' + jobAccessSearch}>
+            Explore industry/sector of employment
+          </Button>
+        </div>
+      </SectionEntry>
+    </Section>,
+
     <Section title="Area Demographics">
       {(() => {
         if (data?.length === 1) {
@@ -587,89 +678,6 @@ function Sections() {
             };
           })}
       />
-    </Section>,
-    <Section title="Work and School">
-      <SectionEntry
-        s={{ gridColumn: '1 / 3' }}
-        m={{ gridColumn: '1 / 4' }}
-        l={{ gridColumn: '1 / 5' }}
-      >
-        <div>
-          <SelectTravelMethod travelMethodList={travelMethodList} />
-        </div>
-      </SectionEntry>
-      <Statistic.Percent
-        wrap
-        label="Trips using public transit"
-        data={data?.map((area) => {
-          const publicTransitTrips =
-            area.statistics?.thursday_trip.methods.commute.public_transit || 0;
-          const allTrips = Object.values(
-            area.statistics?.thursday_trip.methods.commute || {}
-          ).reduce((sum, value) => sum + (value || 0), 0);
-
-          return {
-            label: area.__label,
-            value: ((publicTransitTrips / allTrips) * 100).toFixed(2),
-          };
-        })}
-      />
-      <Statistic.Percent
-        wrap
-        label="Trips that could use public transit"
-        data={data?.map((area) => {
-          const possibleConversions =
-            area.statistics?.thursday_trip.possible_conversions.via_walk || 0;
-          const allTrips = Object.values(area.statistics?.thursday_trip.methods.__all || {}).reduce(
-            (sum, value) => sum + (value || 0),
-            0
-          );
-
-          return {
-            label: area.__label,
-            value: ((possibleConversions / allTrips) * 100).toFixed(2),
-          };
-        })}
-      />
-      <Statistic.Percent
-        wrap
-        label="Households without a vehicle (ACS)"
-        data={data?.map((area) => {
-          const households =
-            area.census_acs_5year
-              ?.map((item) => item.households__total)
-              .reduce((sum, value) => sum + (value || 0), 0) || NaN;
-          const householdsNoVehicle =
-            area.census_acs_5year
-              ?.map((item) => item.households__no_vehicle)
-              .reduce((sum, value) => sum + (value || 0), 0) || NaN;
-
-          return {
-            label: area.__label,
-            value: ((householdsNoVehicle / households) * 100).toFixed(2),
-          };
-        })}
-      />
-      <Statistic.Number
-        wrap
-        label="Median commute time (all modes)"
-        unit="minutes"
-        data={data?.map((area) => {
-          const medianDuration = area.statistics?.thursday_trip.median_duration.commute || 0;
-          return { label: area.__label, value: medianDuration.toFixed(2) };
-        })}
-      />
-      <SectionEntry
-        s={{ gridColumn: '1 / 3' }}
-        m={{ gridColumn: '1 / 4' }}
-        l={{ gridColumn: '1 / 5' }}
-      >
-        <div>
-          <Button href={'#/job-access' + jobAccessSearch}>
-            Explore industry/sector of employment
-          </Button>
-        </div>
-      </SectionEntry>
     </Section>,
   ].filter(notEmpty);
 }
