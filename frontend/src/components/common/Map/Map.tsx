02157d75bee15c9054f0a7531c2b4cf05aa54d77
Jack Buehner <jackb@furman.edu>
2025-08-28 12:06:20 -0400

    feat(frontend): implement Vanessa's paratransit and walkshed icons
    Clsoes #105

--- frontend/src/components/common/Map/Map.tsx
+++ frontend/src/components/common/Map/Map.tsx
@@ -15,6 +15,8 @@ import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import { debounce, notEmpty } from '../../../utils';
 import { Button } from '../Button/Button';
 import { IconButton } from '../IconButton/IconButton';
+import { ParatransitIcon } from '../IconButton/ParatransitIcon';
+import { WalkshedIcon } from '../IconButton/WalkshedIcon';
 import type { GeoJSONLayerInit } from './types';
 
 interface MapProps {
@@ -252,17 +254,7 @@ export function Map(props: MapProps) {
                   onClick={() => toggleServiceAreaLayer('walk')}
                   active={serviceAreaLayers.walk.every((layer) => layer.visible)}
                 >
-                  <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M11.998 14a2 2 0 1 0 0-4 2 2 0 0 0 0 4Z" fill="currentColor" />
-                    <path
-                      d="M5.998 12a6 6 0 1 1 12 0 6 6 0 0 1-12 0Zm6-4.5a4.5 4.5 0 1 0 0 9 4.5 4.5 0 0 0 0-9Z"
-                      fill="currentColor"
-                    />
-                    <path
-                      d="M1.996 12c0-5.524 4.478-10.002 10.002-10.002C17.522 1.998 22 6.476 22 12c0 5.524-4.478 10.002-10.002 10.002-5.524 0-10.002-4.478-10.002-10.002Zm10.002-8.502a8.502 8.502 0 1 0 0 17.004 8.502 8.502 0 0 0 0-17.004Z"
-                      fill="currentColor"
-                    />
-                  </svg>
+                  <WalkshedIcon />
                 </IconButton>
               </arcgis-placement>
             ) : null}
@@ -289,16 +281,7 @@ export function Map(props: MapProps) {
                   onClick={() => toggleServiceAreaLayer('paratransit')}
                   active={serviceAreaLayers.paratransit.every((layer) => layer.visible)}
                 >
-                  <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                    <path
-                      d="M8 16a1 1 0 1 0 0-2 1 1 0 0 0 0 2ZM17 15a1 1 0 1 1-2 0 1 1 0 0 1 2 0ZM10.75 5a.75.75 0 0 0 0 1.5h2.5a.75.75 0 0 0 0-1.5h-2.5Z"
-                      fill="currentColor"
-                    />
-                    <path
-                      d="M7.75 2A3.75 3.75 0 0 0 4 5.75V9.5H2.75a.75.75 0 1 0 0 1.5H4v8.75c0 .966.783 1.75 1.75 1.75h1.5A1.75 1.75 0 0 0 9 19.75V18.5h6v1.25c0 .966.784 1.75 1.75 1.75h1.5A1.75 1.75 0 0 0 20 19.75V11h1.227a.75.75 0 0 0 0-1.5H20V5.75A3.75 3.75 0 0 0 16.25 2h-8.5ZM18.5 18.5v1.25a.25.25 0 0 1-.25.25h-1.5a.25.25 0 0 1-.25-.25V18.5h2Zm0-1.5h-13v-4h13v4Zm-13 2.75V18.5h2v1.25a.25.25 0 0 1-.25.25h-1.5a.25.25 0 0 1-.25-.25Zm0-14A2.25 2.25 0 0 1 7.75 3.5h8.5a2.25 2.25 0 0 1 2.25 2.25v5.75h-13V5.75Z"
-                      fill="currentColor"
-                    />
-                  </svg>
+                  <ParatransitIcon />
                 </IconButton>
               </arcgis-placement>
             ) : null}
